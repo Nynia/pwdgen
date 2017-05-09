ad1ba7dfc9614eccff89f52f65fb5536013a5550
Nynia <jokeforce@gmail.com>
2017-05-09 14:27:39 +0800

    add app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+# -*- coding:utf8 -*-
+from flask import Flask, request, make_response
+import hashlib
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def wechat_auth():
+    if request.method == 'GET':
+        token = 'cclove'
+        query = request.args
+        signature = query.get('signature', '')
+        timestamp = query.get('timestamp', '')
+        nonce = query.get('nonce', '')
+        echostr = query.get('echostr', '')
+        s = [timestamp, nonce, token]
+        s.sort()
+        s = ''.join(s)
+        if (hashlib.sha1(s).hexdigest() == signature):
+            return make_response(echostr)
