cb837f5e95eddc3419179b61a325719a6a3849ca
Nynia <jokeforce@gmail.com>
2017-05-09 15:10:32 +0800

    debug

--- pwdgen.py
+++ pwdgen.py
@@ -1,7 +1,7 @@
 # -*- coding:utf8 -*-
 from flask import Flask, request, make_response
-import hashlib
-
+import hashlib,time
+import xml.etree.ElementTree as ET
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -19,3 +19,13 @@ def wechat_auth():
         if (hashlib.sha1(s).hexdigest() == signature):
             print signature
             return make_response(echostr)
+    else:
+        rec = request.stream.read()
+        xml_rec = ET.fromstring(rec)
+        tou = xml_rec.find('ToUserName').text
+        fromu = xml_rec.find('FromUserName').text
+        content = xml_rec.find('Content').text
+        xml_rep = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>"
+        response = make_response(xml_rep % (fromu, tou, str(int(time.time())), content))
+        response.content_type = 'application/xml'
+        return response
