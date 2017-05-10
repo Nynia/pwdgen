57d13d92e5942ed27b7b11d2ea3afae5eadcc128
Nynia <jokeforce@gmail.com>
2017-05-10 13:40:38 +0800

    debug

--- pwdgen.py
+++ pwdgen.py
@@ -30,7 +30,7 @@ def wechat_auth():
         fromuser = xml_rec.find('FromUserName').text
         content = xml_rec.find('Content').text
         print [touser,fromuser,content]
-        password = gen_password(content,sumof(fromuser+touser)
+        password = gen_password(content,sumof(fromuser+touser))
         xml_rep = '''<xml>
             <ToUserName><![CDATA[%s]]></ToUserName>
             <FromUserName><![CDATA[%s]]></FromUserName>
